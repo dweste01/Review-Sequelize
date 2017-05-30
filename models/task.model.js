e3d64be08b293f5dea468bd39dc1da1242a4321a
Danielle Westerman <daniellepwesterman@gmail.com>
2017-05-30 11:19:10 -0400

    final submission

--- models/task.model.js
+++ models/task.model.js
@@ -20,7 +20,71 @@ var Task = db.define('Task', {
   due: Sequelize.DATE
 }, {
   //---------VVVV---------  your code below  ---------VVV----------
+  getterMethods: {
+    timeRemaining: function() {
+      if (!this.due) {
+        return Infinity;
+      } else {
+        return (this.due - Date.now())
+      }
+    },
 
+    overdue: function() {
+      return (Date.now() > this.due) && !this.complete;
+    }
+  },
+
+  classMethods: {
+    clearCompleted: function() {
+        return Task.destroy({
+          where: {
+            complete: true
+          }
+        });
+      },
+
+      completeAll: function() {
+        return Task.update(
+                { complete: true},
+                { where: { complete: false } } )
+      }
+    },
+
+    instanceMethods: {
+      addChild: function(nameObj) {
+        return Task.create(nameObj)
+                .then(child => {
+                  return child.setParent(this);
+                })
+      },
+
+      getChildren: function() {
+        return Task.findAll({
+          where: {
+            parentId: this.id
+          }
+        })
+      },
+
+      getSiblings: function() {
+        return Task.findAll({
+          where: {
+            parentId: this.parentId,
+            id : {$ne: this.id}
+          }
+        })
+      }
+    },
+
+    hooks: {
+      beforeDestroy: function(task, opts) {
+        return Task.destroy({
+          where: {
+            parentId: task.id
+          }
+        })
+      }
+    }
 
 
 
